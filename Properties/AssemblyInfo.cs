5476af6f585f044e03e20ef7a3c0753f3b8857dc
emmandesu <61533468+emmandesu@users.noreply.github.com>
2026-04-14 07:30:33 +0200

    chore(version): bump assembly metadata to v1.6.1
    update assembly version to 1.6.1.0
    update file version to 1.6.1.0
    add informational version 1.6.1 for product metadata display
    clean up product metadata fields for release consistency
    fix copyright text encoding

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,19 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DISM Tool GUI")]
-[assembly: AssemblyDescription("Graphical frontend for DISM and SFC repair operations")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("A Windows desktop utility for DISM and SFC repair, package management, WIM servicing, and maintenance operations.")]
+[assembly: AssemblyConfiguration("Release")]
+
 [assembly: AssemblyCompany("Emmanuel Flores")]
-[assembly: AssemblyProduct("DISM Tool GUI")] // Shows under Product Name
+[assembly: AssemblyProduct("DISM Tool GUI")]
 [assembly: AssemblyCopyright("© 2025 Emmanuel Flores")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("DISM Tool GUI")]
+
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("d9e8a1f6-4bb3-49e2-93cb-65aa76f6c25c")] // Random GUID is okay
-[assembly: AssemblyVersion("1.5.8.0")]
-[assembly: AssemblyFileVersion("1.5.8.0")]
+[assembly: Guid("d9e8a1f6-4bb3-49e2-93cb-65aa76f6c25c")]
+
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyInformationalVersion("1.6.1")]
