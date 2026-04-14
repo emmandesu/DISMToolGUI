0800345d93faf74886543ce19e970c3aa61c5d26
emmandesu <61533468+emmandesu@users.noreply.github.com>
2026-04-14 07:27:10 +0200

    chore(settings): clean up application settings accessors
    simplify settings property access using nameof(...)
    keep LicenseAccepted under strongly typed user-scoped settings
    improve maintainability by removing hardcoded property key strings

--- Settings.cs
+++ Settings.cs
@@ -1,19 +1,20 @@
-﻿using System.Configuration;
+using System.Configuration;
 
 namespace DismToolGui
 {
     internal sealed partial class Settings : ApplicationSettingsBase
     {
-        private static readonly Settings defaultInstance = (Settings)Synchronized(new Settings());
+        private static readonly Settings defaultInstance =
+            (Settings)Synchronized(new Settings());
 
         public static Settings Default => defaultInstance;
 
-        [UserScopedSetting()]
+        [UserScopedSetting]
         [DefaultSettingValue("False")]
         public bool LicenseAccepted
         {
-            get => (bool)this["LicenseAccepted"];
-            set => this["LicenseAccepted"] = value;
+            get => (bool)this[nameof(LicenseAccepted)];
+            set => this[nameof(LicenseAccepted)] = value;
         }
     }
 }
